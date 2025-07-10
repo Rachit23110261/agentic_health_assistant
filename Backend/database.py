25880f4c867d8a2327c33ad65cb2c152c5eb47b1
rachit mehta <23110261@iitgn.ac.in>
2025-07-10 12:23:46 +0530

    final

--- Backend/database.py
+++ Backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from contextlib import asynccontextmanager
 
-DATABASE_URL = "postgresql+asyncpg://doc_user:doc_pass@localhost/doc_app"
+DATABASE_URL = "postgresql://agentic_db_85if_user:VOSG2pTdIuFsNNfPjWFH8Lde7W2R3r3F@dpg-d1nm6eruibrs7387stj0-a/agentic_db_85if"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
